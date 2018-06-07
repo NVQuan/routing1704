9e2f5f46b8d574271b1c84a5f6f228448ea13386
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-07 21:12:04 +0700

    routerLinkOtions

--- src/app/components/nav.component.ts
+++ src/app/components/nav.component.ts
@@ -6,19 +6,20 @@ import { Component } from '@angular/core';
     <nav class="navbar navbar-default" style="margin-top: 10px">
         <div class="container-fluid">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Khoa Pham Page</a>
+            <a class="navbar-brand">Khoa Pham Page</a>
           </div>
           <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/signin">Sign In</a></li>
-            <li><a href="/signup">Sign Up</a></li>
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/friend">Friend</a></li>
-            <li><a href="/friend">Log out</a></li>
+            <li><a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
+            <li><a routerLink="/signin" routerLinkActive="active">Sign In</a></li>
+            <li><a routerLink="/signup" routerLinkActive="active">Sign Up</a></li>
+            <li><a routerLink="/profile" routerLinkActive="active">Profile</a></li>
+            <li><a routerLink="/friend" routerLinkActive="active">Friend</a></li>
+            <li><a>Log out</a></li>
           </ul>
         </div>
       </nav>
-    `
+    `,
+    styles: [`a.active { font-weight: bold; }`]
 })
 
 export class NavComponent {}
