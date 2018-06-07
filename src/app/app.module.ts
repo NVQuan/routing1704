388e802a33596aed65b5198052ca805257053e90
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-07 21:04:01 +0700

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FriendComponent } from './components/friend.component';
@@ -8,6 +9,14 @@ import { ProfileComponent } from './components/profile.component';
 import { SignUpComponent } from './components/sign-up.component';
 import { SignInComponent } from './components/sign-in.component';
 import { NavComponent } from './components/nav.component';
+
+const routesConfig: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'friend', component: FriendComponent },
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +29,8 @@ import { NavComponent } from './components/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routesConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
