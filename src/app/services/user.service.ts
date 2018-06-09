654b3cc79f274889ffea0f1848b7c666297c8b2a
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-09 20:09:24 +0700

    save token after sign in

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { RequestService } from './request.service';
 
 @Injectable()
 
 export class UserService {
-    constructor(private request: RequestService) {}
+    constructor(private request: RequestService, private router: Router) {}
 
     signIn(email: string, password: string) {
         this.request.post('/user/signin', { email, password })
-        .then(response => alert(JSON.stringify(response)))
-        .catch(error => alert(JSON.stringify(error)));
+        .then(response => {
+            localStorage.setItem('token', response.user.token);
+            this.router.navigate(['/profile']);
+        })
+        .catch(error => alert('Kiểm tra lại thông tin đăng nhập.'));
     }
 }
