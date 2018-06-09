30984db2f60dab2700c1457ea9ae45e39a7fbbcb
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-09 20:20:49 +0700

    show linkds

--- src/app/components/nav.component.ts
+++ src/app/components/nav.component.ts
@@ -9,13 +9,16 @@ import { UserService } from '../services/user.service';
           <div class="navbar-header">
             <a class="navbar-brand">Khoa Pham Page</a>
           </div>
-          <ul class="nav navbar-nav">
+          <ul class="nav navbar-nav" *ngIf="isSignedIn">
             <li><a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
-            <li><a routerLink="/signin" routerLinkActive="active">Sign In</a></li>
-            <li><a routerLink="/signup" routerLinkActive="active">Sign Up</a></li>
             <li><a routerLink="/profile" routerLinkActive="active">Profile</a></li>
             <li><a routerLink="/friend" routerLinkActive="active">Friend</a></li>
             <li><a (click)="logOut();" style="cursor: pointer;">Log out</a></li>
+          </ul>
+          <ul class="nav navbar-nav" *ngIf="!isSignedIn">
+            <li><a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
+            <li><a routerLink="/signin" routerLinkActive="active">Sign In</a></li>
+            <li><a routerLink="/signup" routerLinkActive="active">Sign Up</a></li>
           </ul>
         </div>
       </nav>
@@ -27,4 +30,6 @@ export class NavComponent {
   constructor(private userService: UserService) {}
 
   logOut() { this.userService.logOut(); }
+
+  get isSignedIn(): boolean { return !!localStorage.getItem('token'); }
 }
